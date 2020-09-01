e1a0666050d6a92772b11ceb8e986b1e2f41aad5
Andrew Falzon <andrew.falzon@bambora.com>
2020-09-01 14:38:19 +1000

    Cart methods and views.

--- RentalPlatform/RentalPlatform/Models/Cart.cs
+++ RentalPlatform/RentalPlatform/Models/Cart.cs
@@ -11,6 +11,7 @@ namespace RentalPlatform.Models
         [Key]
         public int Id { get; set; }
         public string UserId { get; set; }
+        public int Quantity { get; set; }
         public virtual Product Product { get; set; }
     }
 }
