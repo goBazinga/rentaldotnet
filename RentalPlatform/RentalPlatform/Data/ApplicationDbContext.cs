ab95b00afe065d1327b9cc4b8f8dabd409b874f6
Andrew Falzon <andrew.falzon@bambora.com>
2020-09-01 12:08:07 +1000

    Seed product data.

--- RentalPlatform/RentalPlatform/Data/ApplicationDbContext.cs
+++ RentalPlatform/RentalPlatform/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RentalPlatform.Models;
@@ -14,6 +15,22 @@ namespace RentalPlatform.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Product>().HasData(
+                new Product()
+                {
+                    Id = 1,
+                    Name = "W-Desk",
+                    Price = 50.00m,
+                    Quantity = 20,
+                    QuantityAvailable = 10
+                }
+            );
+
+            base.OnModelCreating(builder);
         }
     }
 }
