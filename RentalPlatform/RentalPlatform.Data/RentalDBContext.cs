1cb8d72b3d76aed67683d6e32b39a50eb8b230ba
Andrew Falzon <andrew.falzon@bambora.com>
2020-09-01 10:45:03 +1000

    Added database.

---
+++ RentalPlatform/RentalPlatform.Data/RentalDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentalPlatform.Data.Model;
+
+namespace RentalPlatform.Data
+{
+    public class RentalDBContext : DbContext
+    {
+        public RentalDBContext(DbContextOptions<RentalDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
