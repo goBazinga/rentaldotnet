281b8d813bd37be5a65d819ecf8aaf8895458460
Andrew Falzon <andrew.falzon@bambora.com>
2020-09-01 12:01:36 +1000

    Added products to db context.

--- RentalPlatform/RentalPlatform/Data/ApplicationDbContext.cs
+++ RentalPlatform/RentalPlatform/Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RentalPlatform.Models;
 
 namespace RentalPlatform.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Product> Products { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ RentalPlatform/RentalPlatform/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentalPlatform.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int QuantityAvailable { get; set; }
+    }
+}
